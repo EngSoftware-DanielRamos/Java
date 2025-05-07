bcd6ad362998449a2761f19e27496d082ee9f871
Daniel Ramos <financdaniel@gmail.com>
2025-05-07 15:23:48 -0300

    Aula finalizadas

--- maratona-java/src/cloud/drsoftware/maratonajava/introducao/Aula05EstruturasCondicionais02.java
+++ maratona-java/src/cloud/drsoftware/maratonajava/introducao/Aula05EstruturasCondicionais02.java
@@ -15,6 +15,8 @@ public class Aula05EstruturasCondicionais02 {
         }else{
             categoria = "Categoria Adulto";
         }
+
+        categoria = idade < 15 ? "Categoria Infantil" : idade >= 15 && idade < 18 ? "Categoria Juvenil" : "Categoria Adulto";
         System.out.println(categoria);
     }
 }
