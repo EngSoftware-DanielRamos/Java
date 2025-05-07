d67409cac7a9a63ff7efc006332feaa1cd11bee7
Daniel Ramos <financdaniel@gmail.com>
2025-05-07 09:08:26 -0300

    Aula 21 finalizada

--- maratona-java/src/cloud/drsoftware/maratonajava/introducao/Aula05EstruturasCondicionais01.java
+++ maratona-java/src/cloud/drsoftware/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -1,17 +1,23 @@
 package cloud.drsoftware.maratonajava.introducao;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args) {
         int idade = 15;
         boolean isAutorizadoComprarBebida = idade >= 18;
         // !
 
-        if(isAutorizadoComprarBebida) {
+        if(isAutorizadoComprarBebida != false) {
             System.out.println("Autorizado a comprar bebida alcólica");
+        }else{
+            System.out.println("Não autorizado a comprar bebida alcólica");
         }
 
         if(!isAutorizadoComprarBebida){
             System.out.println("Não autorizado a comprar bebida alcólica");
+        }
+        boolean c = false;
+        if(c == true){
+            System.out.println("Dentro de algo que nunca deve ser feito");
         }
 
         System.out.println("Fora do if");
