ba98baf83aae6fb556608a9ed8ab987ce5fc559e
Daniel Ramos <financdaniel@gmail.com>
2025-05-12 15:21:43 -0300

    47 - Orientação Objetos - Métodos pt 04 - Retorno pt 02

---
+++ maratona-java/src/cloud/drsoftware/maratonajava/javacore/Aintroducaoclasses/dominio/Carro.java
@@ -0,0 +1,7 @@
+package cloud.drsoftware.maratonajava.javacore.Aintroducaoclasses.dominio;
+
+public class Carro {
+    public String nome;
+    public String modelo;
+    public int ano;
+}
---
+++ maratona-java/src/cloud/drsoftware/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -0,0 +1,12 @@
+package cloud.drsoftware.maratonajava.javacore.Bintroducaometodos.test;
+
+import cloud.drsoftware.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        double result = calculadora.divideDoisNumeros(20,2);
+        System.out.println(result);
+        System.out.println(calculadora.divideDoisNumeros(20,0));
+    }
+}
