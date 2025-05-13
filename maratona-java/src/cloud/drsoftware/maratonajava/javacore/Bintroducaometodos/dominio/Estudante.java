da838704e3f2fc29506682daa24881b89d0d3dad
Daniel Ramos <financdaniel@gmail.com>
2025-05-13 09:17:08 -0300

    Aula 49 - Orientação Objetos - Métodos pt 06 - Parâmetros tipo referência

---
+++ maratona-java/src/cloud/drsoftware/maratonajava/javacore/Bintroducaometodos/dominio/Estudante.java
@@ -0,0 +1,7 @@
+package cloud.drsoftware.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Estudante {
+    public String nome;
+    public int idade;
+    public char sexo;
+}
---
+++ maratona-java/src/cloud/drsoftware/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest04.java
@@ -0,0 +1,15 @@
+package cloud.drsoftware.maratonajava.javacore.Bintroducaometodos.test;
+
+import cloud.drsoftware.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest04 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int a = 1;
+        int b = 2;
+        calculadora.alteraDoisNumeros(a, b);
+        System.out.println("CalculadoraTest04");
+        System.out.println("Num1 "+a);
+        System.out.println("Num2 "+b);
+    }
+}
